e451021ffde410ed054a10dafbc2300512235b2f
Ignaz Kevenaar <ignazkevenaar@gmail.com>
2025-01-22 07:56:26 +0100

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
 <script setup>
+import { ref } from 'vue';
 import LicencePlate from "./components/LicencePlate.vue";
+import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
+
+const plate = ref(null);
+
+const render = () => {
+  var node = plate.value.$el;
+
+  toPng(node)
+    .then(function (dataUrl) {
+      var img = new Image();
+      img.src = dataUrl;
+      document.body.appendChild(img);
+    })
+    .catch(function (error) {
+      console.error('oops, something went wrong!', error);
+    });
+};
 </script>
 
 <template>
   <div>
-    <LicencePlate />
+    <LicencePlate ref="plate" />
+    <button @click="render">Render that up bro</button>
   </div>
 </template>
 
